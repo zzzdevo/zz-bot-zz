e009655dd7f0b9eb13aa6a380cf5e9852cdf2a8b
aizeno <110237161+aizeno@users.noreply.github.com>
2023-07-23 14:20:45 +0300

    Update mute.py

--- AnonX/plugins/mute.py
+++ AnonX/plugins/mute.py
@@ -34,5 +34,13 @@ async def mute_admin(cli, message: Message, _, chat_id):
 & filters.forwarded
 )
 @AdminRightsCheck
-def gjgh(client, m):
+def gjgh(client, message, m):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id in message:
+            return await message.reply_text(f" {message.from_user.mention} پێشتر چالاککراوە  ")
+        message.append(message.chat.id)
+        return await message.reply_text(f"چالاککراوا  ←{message.from_user.mention}")
+    else:
+        return await message.reply_text(f" {message.from_user.mention} تۆ ئەدمحن نییت ")
     m.delete()
