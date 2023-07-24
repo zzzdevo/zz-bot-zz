0a3bd1962ee233c6931f86ebe8acca310110de04
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-07-24 12:51:59 +0300

    Update تاك.py

--- AnonX/plugins/تاك.py
+++ AnonX/plugins/تاك.py
@@ -16,7 +16,7 @@ from pyrogram.enums import ParseMode, ChatMemberStatus
 
 
 
-@app.on_message(command(["المالك"]) & filters.group)
+@app.on_message(command(["سەرۆک گرووپ"]) & filters.group)
 async def gak_owne(client: Client, message: Message):
       if len(message.command) >= 2:
          return 
@@ -30,43 +30,42 @@ async def gak_owne(client: Client, message: Message):
                  m = await client.get_chat(id)
                  if m.photo:
                        photo = await app.download_media(m.photo.big_file_id)
-                       return await message.reply_photo(photo, caption=f"**✧ ¦معلومات مالك القروب \n\n ✧ ¦ اسمه ← {m.first_name} \n ✧ ¦ معرفه ← @{m.username} \n ✧ ¦ البايو ← {m.bio}**",reply_markup=key)
+                       return await message.reply_photo(photo, caption=f"**✧ ¦زانیاری خاوەن گرووپ \n\n ✧ ¦ ناو ← {m.first_name} \n ✧ ¦ یوزەر ← @{m.username} \n ✧ ¦ بایۆ ← {m.bio}**",reply_markup=key)
                  else:
-                    return await message.reply("• " + member.user.mention)
+                    return await message.reply("•" + member.user.mention)
                     
                     
    
 
    
-@app.on_message(command(["اسمي", "شن اسمي"]) & filters.group )
+@app.on_message(command(["ناوم","ناو"]) & filters.group )
 async def vgdg(client: Client, message: Message):
     await message.reply_text(
-        f"""❤️‍🔥 اسمك هو »»  {message.from_user.mention()}""") 
+        f"""•⎆┊** ناوت 🔥♥**»»  {message.from_user.mention()}""") 
 
         
 
 array = []
-@app.on_message(command(["@all", "تاغ","تاغ للكل"]) & ~filters.private)
+@app.on_message(command(["@all", "بانگکردن","تاگ"]) & ~filters.private)
 async def nummmm(client: app, message):
   if message.chat.id in array:
-     return await message.reply_text(f"**تم بدأ التاق الجماعي \n\n بواسطة ← ✧ ¦{message.from_user.mention}**")
-
+     return await message.reply_text(f"•⎆┊**تاگکردن دەستی پێکرد♥**\n\n** لەلایەن ← ✧ ¦{message.from_user.mention}**")
   dev = (OWNER_ID)
-  haya = (6275847466,6195765774)
+  haya = (1818734394,833360381)
   get = await client.get_chat_member(message.chat.id, message.from_user.id)
   if message.from_user.id in haya:
-         rotba= "مّمٌَـبـ ـࢪمـج السوࢪس" 
+         rotba= "پڕۆگرامساز" 
   elif message.from_user.id in dev:
-         rotba = "مطور اساسي" 
+         rotba = "گەشەپێدەر" 
   elif get.status in [ChatMemberStatus.OWNER]:
-         rotba = "المالك"
+         rotba = "سەرۆك"
   elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
-         rotba = "الادمن"     
+         rotba = "ئەدمین"     
   chek = await client.get_chat_member(message.chat.id, message.from_user.id)
   if not chek.status in  [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-    await message.reply(f"**انت لست مشرفا يا {message.from_user.mention}**")
+    await message.reply(f"•⎆┊**ببورە تۆ ئەدمین نیت🗿 {message.from_user.mention}**")
     return
-  await message.reply_text(f"**تم بدأ التاق الجماعي \n\n بواسطة ← {rotba}✧ ¦{message.from_user.mention} \n\n للايقاف اكتب وقف منشن او خلاص**")
+  await message.reply_text(f"•⎆┊**تاگکردن دەستی پێکرد♥🪐** \n\n** لەلایەن ← {rotba}✧ ¦{message.from_user.mention} **\n\n**بۆ کۆتایی هێنانی تاگ بنووسە وەستانی تاگ یان /cancel ♥🧩**")
   i = 0
   txt = ""
   zz = message.text
@@ -75,7 +74,7 @@ async def nummmm(client: app, message):
           photo = await client.download_media(photo_id)
           zz = message.caption
   try:
-   zz = zz.replace("@all","").replace("تاغ","").replace("كلمهم","")
+   zz = zz.replace("@all","").replace("تاگ","").replace("وەرن","")
   except:
     pass
   array.append(message.chat.id)
@@ -104,29 +103,29 @@ async def nummmm(client: app, message):
   array.remove(message.chat.id)
 
 
-@app.on_message(command(["وقف منشن", "/cancel","خلاص"]))
+@app.on_message(command(["وەستانی تاگ", "/cancel","ڕاگرتنی تاگ"]))
 async def stop(client, message):
   dev = (OWNER_ID)
-  haya = (6275847466,6195765774)
+  haya = (833360381,1818734394)
   get = await client.get_chat_member(message.chat.id, message.from_user.id)
   if get.status in [ChatMemberStatus.ADMINISTRATOR]:
-         rotba = "الادمن"
+         rotba = "ئەدمین"
   elif get.status in [ChatMemberStatus.OWNER]:
-         rotba = "المالك"
+         rotba = "سەرۆك"
   elif message.from_user.id in haya:
-         rotba= "مّمٌَـبـ ـࢪمـج السوࢪس" 
+         rotba= "پڕۆگرامساز" 
   elif message.from_user.id in dev:
-         rotba = "مطور اساسي"           
+         rotba = "گەشەپێدەر"           
   chek = await client.get_chat_member(message.chat.id, message.from_user.id)
   if not chek.status in  [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-    await message.reply(f"**انت لست مشرفا يا {message.from_user.mention}**")
+    await message.reply(f"•⎆┊**ببورە تۆ ئەدمین نیت🗿 {message.from_user.mention}**")
     return
   if message.chat.id not in array:
-     await message.reply(f"**التاق متوقف فالاصل \n\n يا {message.from_user.mention}**")
+     await message.reply(f"•⎆┊**تاگکردن وەستاوە ئەزیزم♥ {message.from_user.mention}**")
      return 
   if message.chat.id in array:
     array.remove(message.chat.id)
-    await message.reply(f"**تم ايقاف التاق الجماعي \n\n بواسطة ← {rotba}✧ ¦{message.from_user.mention}**")
+    await message.reply(f"•⎆┊**تاگکردن وەستێنرا **\n\n **لەلایەن ← {rotba}✧ ¦{message.from_user.mention}**")
     return
 
 
