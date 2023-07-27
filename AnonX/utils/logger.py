e18fa753d2044b53f947214fb08361b0f871916c
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-07-27 22:52:29 +0300

    Update logger.py

--- AnonX/utils/logger.py
+++ AnonX/utils/logger.py
@@ -2,19 +2,25 @@ from config import LOG, LOG_GROUP_ID, MUSIC_BOT_NAME
 from AnonX import app
 from AnonX.utils.database import is_on_off
 
+
 async def play_logs(message, streamtype):
     if await is_on_off(LOG):
         if message.chat.username:
             chatusername = f"@{message.chat.username}"
         else:
-            chatusername = "**چاتی تایبەت**"
-        logger_text = f"""**[ᯓ 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝙌 - ئاماری پەخشکردن](https://t.me/MGIMT)\n••┉┉┉┉┉┉┉••🝢••┉┉┉┉┉┉┉••\n
-**گرووپ:** {message.chat.title} [`{message.chat.id}`]
-**یوزەر:** @{message.from_user.username}
-**ناسنامە:** `{message.from_user.id}`
-**بەستەر چات:** {chatusername}
-**گەڕان کرا لەلایەن:** {message.text}
-**جۆری پەخشکردن:** {streamtype}"""
+            chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        logger_text = f"""
+**{MUSIC_BOT_NAME} ᴩʟᴀʏ ʟᴏɢɢᴇʀ**
+
+**ᴄʜᴀᴛ:** {message.chat.title} [`{message.chat.id}`]
+**ᴜsᴇʀ:** {message.from_user.mention}
+**ᴜsᴇʀɴᴀᴍᴇ:** @{message.from_user.username}
+**ɪᴅ:** `{message.from_user.id}`
+**ᴄʜᴀᴛ ʟɪɴᴋ:** {chatusername}
+
+**sᴇᴀʀᴄʜᴇᴅ ғᴏʀ:** {message.text}
+
+**sᴛʀᴇᴀᴍ ᴛʏᴩᴇ:** {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
