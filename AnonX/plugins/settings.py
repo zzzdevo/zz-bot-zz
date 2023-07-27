55ffa07e3aec51b9ebe185d3badf575d0a6d9a81
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-07-28 00:00:02 +0300

    Update settings.py

--- AnonX/plugins/settings.py
+++ AnonX/plugins/settings.py
@@ -1,4 +1,3 @@
-
 from pyrogram import filters
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
@@ -19,12 +18,10 @@ from AnonX.utils.database import (add_nonadmin_chat,
                                        is_cleanmode_on,
                                        is_commanddelete_on,
                                        is_nonadmin_chat,
-                                       is_suggestion,
                                        remove_nonadmin_chat,
                                        save_audio_bitrate,
                                        save_video_bitrate,
-                                       set_playmode, set_playtype,
-                                       suggestion_off, suggestion_on)
+                                       set_playmode, set_playtype)
 from AnonX.utils.decorators.admins import ActualAdminCB
 from AnonX.utils.decorators.language import language, languageCB
 from AnonX.utils.inline.settings import (
@@ -125,7 +122,7 @@ async def gen_buttons_vid(_, aud):
 
 @app.on_callback_query(
     filters.regex(
-        pattern=r"^(SEARCHANSWER|PLAYMODEANSWER|PLAYTYPEANSWER|AUTHANSWER|CMANSWER|COMMANDANSWER|SUGGANSWER|CM|AQ|VQ|PM|AU)$"
+        pattern=r"^(SEARCHANSWER|PLAYMODEANSWER|PLAYTYPEANSWER|AUTHANSWER|CMANSWER|COMMANDANSWER|CM|AQ|VQ|PM|AU)$"
     )
     & ~BANNED_USERS
 )
@@ -175,13 +172,6 @@ async def without_Admin_rights(client, CallbackQuery, _):
             )
         except:
             return
-    if command == "SUGGANSWER":
-        try:
-            return await CallbackQuery.answer(
-                _["setting_16"], show_alert=True
-            )
-        except:
-            return
     if command == "CM":
         try:
             await CallbackQuery.answer(_["set_cb_5"], show_alert=True)
@@ -193,11 +183,8 @@ async def without_Admin_rights(client, CallbackQuery, _):
             cle = True
         if await is_commanddelete_on(CallbackQuery.message.chat.id):
             sta = True
-        sug = None
-        if await is_suggestion(CallbackQuery.message.chat.id):
-            sug = True
         buttons = cleanmode_settings_markup(
-            _, status=cle, dels=sta, sug=sug
+            _, status=cle, dels=sta
         )
     if command == "AQ":
         try:
@@ -490,7 +477,7 @@ async def authusers_mar(client, CallbackQuery, _):
 
 @app.on_callback_query(
     filters.regex(
-        pattern=r"^(CLEANMODE|COMMANDELMODE|SUGGESTIONCHANGE)$"
+        pattern=r"^(CLEANMODE|COMMANDELMODE)$"
     )
     & ~BANNED_USERS
 )
@@ -505,9 +492,6 @@ async def cleanmode_mark(client, CallbackQuery, _):
         sta = None
         if await is_commanddelete_on(CallbackQuery.message.chat.id):
             sta = True
-        sug = None
-        if await is_suggestion(CallbackQuery.message.chat.id):
-            sug = True
         cle = None
         if await is_cleanmode_on(CallbackQuery.message.chat.id):
             await cleanmode_off(CallbackQuery.message.chat.id)
@@ -515,7 +499,7 @@ async def cleanmode_mark(client, CallbackQuery, _):
             await cleanmode_on(CallbackQuery.message.chat.id)
             cle = True
         buttons = cleanmode_settings_markup(
-            _, status=cle, dels=sta, sug=sug
+            _, status=cle, dels=sta
         )
         return await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
@@ -525,32 +509,13 @@ async def cleanmode_mark(client, CallbackQuery, _):
         sta = None
         if await is_cleanmode_on(CallbackQuery.message.chat.id):
             cle = True
-        sug = None
-        if await is_suggestion(CallbackQuery.message.chat.id):
-            sug = True
         if await is_commanddelete_on(CallbackQuery.message.chat.id):
             await commanddelete_off(CallbackQuery.message.chat.id)
         else:
             await commanddelete_on(CallbackQuery.message.chat.id)
             sta = True
         buttons = cleanmode_settings_markup(
-            _, status=cle, dels=sta, sug=sug
-        )
-    if command == "SUGGESTIONCHANGE":
-        cle = None
-        sta = None
-        if await is_cleanmode_on(CallbackQuery.message.chat.id):
-            cle = True
-        if await is_commanddelete_on(CallbackQuery.message.chat.id):
-            sta = True
-        if await is_suggestion(CallbackQuery.message.chat.id):
-            await suggestion_off(CallbackQuery.message.chat.id)
-            sug = False
-        else:
-            await suggestion_on(CallbackQuery.message.chat.id)
-            sug = True
-        buttons = cleanmode_settings_markup(
-            _, status=cle, dels=sta, sug=sug
+            _, status=cle, dels=sta
         )
     try:
         return await CallbackQuery.edit_message_reply_markup(
