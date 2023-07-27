3f2c08d2f94f7a92686d5b195251aa025d317aa6
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-07-27 21:27:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -92,11 +92,11 @@ TELEGRAM_AUDIO_URL = "https://graph.org/file/4411bac56526350985417.jpg"
 
 TELEGRAM_VIDEO_URL = "https://graph.org/file/4411bac56526350985417.jpg"
 
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/4411bac56526350985417.jpg"
 
 STREAM_IMG_URL = "https://graph.org/file/4411bac56526350985417.jpg"
 
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/4411bac56526350985417.jpg"
 
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 
