601cf4fdc86679e7f6d479010410de25a5d868ce
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-07-30 18:02:20 +0300

    Add files via upload

---
+++ AnonX/plugins/pause.py
@@ -0,0 +1,51 @@
+from strings.filters import command
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from AnonX import app
+from AnonX.core.call import Anon
+from AnonX.utils.database import set_loop
+from AnonX.utils.decorators import AdminRightsCheck, AdminRightsCheckCB
+
+# Commands
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
+
+
+@app.on_message(
+    command(PAUSE_COMMAND)
+    & filters.group
+
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    await Anon.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_9"].format(message.from_user.mention)
+    )
+
+
+@app.on_message(
+    command(PAUSE_COMMAND)
+    & filters.channel
+    & ~BANNED_USERS
+)
+@AdminRightsCheckCB
+async def stop_music_ch(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    await Anon.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    if message.sender_chat:
+        mention = f'<a href=tg://user?id={message.chat.id}>{message.chat.title}</a>'
+    else:
+        mention = message.from_user.mention
+    await message.reply_text(
+        _["admin_9"].format(mention)
+    )
