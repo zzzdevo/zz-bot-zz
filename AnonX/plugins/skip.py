6d0cc82705a8ad4b3eabcabe9173ac67899dcada
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-07 10:33:02 +0300

    Update skip.py

--- AnonX/plugins/skip.py
+++ AnonX/plugins/skip.py
@@ -22,7 +22,7 @@ SKIP_COMMAND = get_command("SKIP_COMMAND")
 @app.on_message(
     filters.command(SKIP_COMMAND)
     & filters.group
-    & ~filters.edited
+    
     & ~BANNED_USERS
 )
 @AdminRightsCheck
