18f33c8a8e0c5526fac8ffdf0fdd4e98cc73c0fd
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-07 16:12:12 +0300

    Update HAYA.py

--- AnonX/plugins/HAYA.py
+++ AnonX/plugins/HAYA.py
@@ -123,6 +123,26 @@ async def yas(client, message):
             ]
         ),
     )
+@app.on_message(
+   command(["کەناڵی بۆت"])
+   
+)
+async def yas(client, message):
+    usr = await client.get_chat(SUPPORT_CHANNEL)
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"**[ᯓ 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝙌 - کەناڵی سەرچاوە 🧑🏻‍💻](t.me/MGIMT)**\n**جۆینی کەناڵی بۆت بکە بۆ بینینی بابەتی جیاوازتر♥**\n\n** بەستەری کەناڵ : https://t.me/{usr.username}**", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ], 
+            ]
+        ),
+    )
+
+
 
 
 @app.on_message(
