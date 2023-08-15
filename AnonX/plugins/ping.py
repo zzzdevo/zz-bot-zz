658f62073c2b0409ab27e4872cf89cd82959a708
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-15 21:20:46 +0300

    Update ping.py

--- AnonX/plugins/ping.py
+++ AnonX/plugins/ping.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
 from AnonX import app
-from AnonX.core.call import Mukesh
+from AnonX.core.call import Anon
 from AnonX.utils import bot_sys_stats
 from AnonX.utils.decorators.language import language
 from AnonX.utils.inline.play import close_keyboard
