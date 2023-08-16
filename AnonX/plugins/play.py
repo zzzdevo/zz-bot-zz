e7e2c504777ffbe2a324b4d156dd9e36acbeef22
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-16 13:31:28 +0300

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -10,21 +10,21 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from MukeshMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from MukeshMusic.core.call import Mukesh
-from MukeshMusic.utils import seconds_to_min, time_to_seconds
-from MukeshMusic.utils.channelplay import get_channeplayCB
-from MukeshMusic.utils.database import is_video_allowed
-from MukeshMusic.utils.decorators.language import languageCB
-from MukeshMusic.utils.decorators.play import PlayWrapper
-from MukeshMusic.utils.formatters import formats
-from MukeshMusic.utils.inline.play import (livestream_markup,
+from AnonX.core.call import Anon
+from AnonX.utils import seconds_to_min, time_to_seconds
+from AnonX.utils.channelplay import get_channeplayCB
+from AnonX.utils.database import is_video_allowed
+from AnonX.utils.decorators.language import languageCB
+from AnonX.utils.decorators.play import PlayWrapper
+from AnonX.utils.formatters import formats
+from AnonX.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from MukeshMusic.utils.inline.playlist import botplaylist_markup
-from MukeshMusic.utils.logger import play_logs
-from MukeshMusic.utils.stream.stream import stream
+from AnonX.utils.inline.playlist import botplaylist_markup
+from AnonX.utils.logger import play_logs
+from AnonX.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
