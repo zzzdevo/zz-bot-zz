604cd6fbc07100351a43b796004c78118f4f8bb4
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-16 13:30:35 +0300

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -1,41 +1,40 @@
 import random
 import string
 from ast import ExceptHandler
-from strings.filters import command
+
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto,
                             Message)
 from pytgcalls.exceptions import NoActiveGroupCall
 
-
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from MukeshMusic import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from AnonX.core.call import Anon
-from AnonX.utils import seconds_to_min, time_to_seconds
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.database import is_video_allowed
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.decorators.play import PlayWrapper
-from AnonX.utils.formatters import formats
-from AnonX.utils.inline.play import (livestream_markup,
+from MukeshMusic.core.call import Mukesh
+from MukeshMusic.utils import seconds_to_min, time_to_seconds
+from MukeshMusic.utils.channelplay import get_channeplayCB
+from MukeshMusic.utils.database import is_video_allowed
+from MukeshMusic.utils.decorators.language import languageCB
+from MukeshMusic.utils.decorators.play import PlayWrapper
+from MukeshMusic.utils.formatters import formats
+from MukeshMusic.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from AnonX.utils.inline.playlist import botplaylist_markup
-from AnonX.utils.logger import play_logs
-from AnonX.utils.stream.stream import stream
+from MukeshMusic.utils.inline.playlist import botplaylist_markup
+from MukeshMusic.utils.logger import play_logs
+from MukeshMusic.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 
 @app.on_message(
-    command(PLAY_COMMAND)
+    filters.command(PLAY_COMMAND)
     & filters.group
-    & ~BANNED_USERS   
-   
+  
+    & ~BANNED_USERS
 )
 @PlayWrapper
 async def play_commnd(
@@ -327,7 +326,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await Anon.stream_call(url)
+                await Mukesh.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(
                     "ᴛʜᴇʀᴇ's ᴀɴ ᴇʀʀᴏʀ ɪɴ ᴛʜᴇ ʙᴏᴛ, ᴩʟᴇᴀsᴇ ʀᴇᴩᴏʀᴛ ɪᴛ ᴛᴏ sᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴀs sᴏᴏɴ ᴀs ᴩᴏssɪʙʟᴇ."
