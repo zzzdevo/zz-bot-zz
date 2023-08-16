c47e919fb69639a34ce1c8c75af1d6277f07a590
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-08-16 21:09:27 +0300

    Update بۆت.py

--- AnonX/plugins/بۆت.py
+++ AnonX/plugins/بۆت.py
@@ -1,47 +1,28 @@
 import asyncio
 
-
 import random
 from AnonX import app
-from pyrogram.types import (InlineKeyboardButton,
-                            InlineKeyboardMarkup, Message)
+from pyrogram.types import Message
 from strings.filters import command
 from pyrogram import filters, Client
-import config
-
 
 
 txt = [
 
-            "**|| @MGIMT - ♥️ هەمیشە خودات لە بیربێت ||**",
+    "**|| @MGIMT - ♥️ هەمیشە خودات لە بیربێت ||**",
+
+    "**||@IQQUR - ♥️ اللهم صَلِّ عَلَى سیدنا مُحَمَّدٍ، وَعَلَى آلِ سیدنا مُحَمَّدٍ ||**",
+
+    "**|| @xv7amo - 🖤👾 هەمیشە بەروو پێشەوە بڕۆ تا دەگەیت بە ئامانجت ||**",
+
+]
 
 
-             "**||@IQQUR - ♥️ اللهم صَلِّ عَلَى سیدنا مُحَمَّدٍ، وَعَلَى آلِ سیدنا مُحَمَّدٍ ||**",
-            
-
-             "**|| @xv7amo - 🖤👾 هەمیشە بەروو پێشەوە بڕۆ تا دەگەیت بە ئامانجت ||**",
-            
-           
- 
-            
-            
-
-        ]
-
-
-        
-
-
-@app.on_message(command(["سلاو","Slaw","slaw","سڵاو","سلام",".","!","بۆت","بوت"]))
-
-
+@app.on_message(command(["سلاو", "Slaw"])
+)
 async def cutt(client: Client, message: Message):
+    a = random.choice(txt)
 
-
-      a = random.choice(txt)
-
-
-      await message.reply(
-
+    await message.reply(
 
         f"{a}")
