cc0ec1508039738122ced349041a44fcc4843c7a
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-12 10:10:50 +0300

    Update autoend.py

--- AnonX/plugins/autoend.py
+++ AnonX/plugins/autoend.py
@@ -13,7 +13,7 @@ AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
 
 @app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
 async def auto_end_stream(client, message):
-    usage = "**ᴜsᴀɢᴇ:**\n\n/autoend [enable|disable]"
+    usage = "**بەکارهێنان:**\n\n/autoend [چالاک|ناچالاک]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip()
