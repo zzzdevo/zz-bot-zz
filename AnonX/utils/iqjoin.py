2f15211ab21cccab4adcc8268a50645771937f4e
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-13 09:50:13 +0300

    Update iqjoin.py

--- AnonX/utils/iqjoin.py
+++ AnonX/utils/iqjoin.py
@@ -3,21 +3,21 @@ from strings.filters import command
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from config import SUPPORT_CHANNEL, CHANNEL_SUDO
-from YukkiMusic import app
+from AnonX import app
 
 
-@app.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
+@app.on_message(& filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not SUPPORT_CHANNEL:  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member(CHANNEL_SUDO, msg.from_user.id)
+            await bot.get_chat_member(SUPPORT_CHANNEL, msg.from_user.id)
         except UserNotParticipant:
             if SUPPORT_CHANNEL.isalpha():
                 link = u"https://t.me/{SUPPORT_CHANNEL}"
             else:
-                chat_info = await bot.get_chat(CHANNEL_SUDO)
+                chat_info = await bot.get_chat(SUPPORT_CHANNEL)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
