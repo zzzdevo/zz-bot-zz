799b6dbb3b29380a14f63fabcdb0fd3de449076c
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-19 16:29:14 +0300

    Update stop.py

--- AnonX/plugins/stop.py
+++ AnonX/plugins/stop.py
@@ -4,7 +4,7 @@ from strings.filters import command
 from config import BANNED_USERS
 from strings import get_command
 from AnonX import app
-from AnonX.core.call import Mukesh
+from AnonX.core.call import Anon
 from AnonX.utils.database import set_loop
 from AnonX.utils.decorators import AdminRightsCheck
 from AnonX.utils.inline.play import close_keyboard
@@ -22,7 +22,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Mukesh.stop_stream(chat_id)
+    await Anon.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.first_name),
