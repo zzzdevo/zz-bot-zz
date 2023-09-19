fd2500fe545b5f101f9f041bfeac668bfbd99b4f
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-19 15:49:32 +0300

    Update admins.py

--- AnonX/utils/decorators/admins.py
+++ AnonX/utils/decorators/admins.py
@@ -1,3 +1,4 @@
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import adminlist
@@ -15,7 +16,11 @@ from ..formatters import int_to_alpha
 
 def AdminRightsCheck(mystic):
     async def wrapper(client, message):
-      
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    "Bot is under maintenance. Please wait for some time..."
+                )
         if await is_commanddelete_on(message.chat.id):
             try:
                 await message.delete()
@@ -31,7 +36,7 @@ def AdminRightsCheck(mystic):
                 [
                     [
                         InlineKeyboardButton(
-                            text="شێوازی چاککردنی",
+                            text="How to Fix this? ",
                             callback_data="AnonymousAdmin",
                         ),
                     ]
@@ -40,7 +45,7 @@ def AdminRightsCheck(mystic):
             return await message.reply_text(
                 _["general_4"], reply_markup=upl
             )
-        if message.command[0][0] == "c" or message.command[0][0] == "#":
+        if message.command[0][0] == "c":
             chat_id = await get_cmode(message.chat.id)
             if chat_id is None:
                 return await message.reply_text(_["setting_12"])
@@ -66,9 +71,13 @@ def AdminRightsCheck(mystic):
     return wrapper
 
 
-def AdminRightsCheckCB(mystic):
+def AdminActual(mystic):
     async def wrapper(client, message):
-     
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    "Bot is under maintenance. Please wait for some time..."
+                )
         if await is_commanddelete_on(message.chat.id):
             try:
                 await message.delete()
@@ -80,70 +89,28 @@ def AdminRightsCheckCB(mystic):
         except:
             _ = get_string("en")
         if message.sender_chat:
-            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-            print(message)
-            chat_id = message.chat.id
-            if chat_id is None:
-                return await message.reply_text(_["setting_12"])
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="How to Fix this? ",
+                            callback_data="AnonymousAdmin",
+                        ),
+                    ]
+                ]
+            )
+            return await message.reply_text(
+                _["general_4"], reply_markup=upl
+            )
+        if message.from_user.id not in SUDOERS:
             try:
-                await app.get_chat(chat_id)
+                member = await app.get_chat_member(
+                    message.chat.id, message.from_user.id
+                )
             except:
-                return await message.reply_text(_["cplay_4"])
-        if message.command[0][0] == "c" or message.command[0][0] == "#":
-            chat_id = await get_cmode(message.chat.id)
-            if chat_id is None:
-                return await message.reply_text(_["setting_12"])
-            try:
-                await app.get_chat(chat_id)
-            except:
-                return await message.reply_text(_["cplay_4"])
-        else:
-            chat_id = message.chat.id
-        if not await is_active_chat(chat_id):
-            return await message.reply_text(_["general_6"])
-        return await mystic(client, message, _, chat_id)
-
-    return wrapper
-
-
-def AdminActual(mystic):
-    async def wrapper(client, message):
-        
-        if await is_commanddelete_on(message.chat.id):
-            try:
-                await message.delete()
-            except:
-                pass
-        try:
-            language = await get_lang(message.chat.id)
-            _ = get_string(language)
-        except:
-            _ = get_string("en")
-        # if message.sender_chat:
-        #     upl = InlineKeyboardMarkup(
-        #         [
-        #             [
-        #                 InlineKeyboardButton(
-        #                     text="شێوازی چاککردنی",
-        #                     callback_data="AnonymousAdmin",
-        #                 ),
-        #             ]
-        #         ]
-        #     )
-        #     return await message.reply_text(
-        #         _["general_4"], reply_markup=upl
-        #     )
-        if not message.sender_chat:
-            if message.from_user.id not in SUDOERS:
-                try:
-                    member = await app.get_chat_member(
-                        message.chat.id, message.from_user.id
-                    )
-                except:
-                    return
-                if not member.can_manage_voice_chats:
-                    return await message.reply(_["general_5"])
-
+                return
+            if not member.can_manage_voice_chats:
+                return await message.reply(_["general_5"])
         return await mystic(client, message, _)
 
     return wrapper
@@ -151,7 +118,12 @@ def AdminActual(mystic):
 
 def ActualAdminCB(mystic):
     async def wrapper(client, CallbackQuery):
-     
+        if await is_maintenance() is False:
+            if CallbackQuery.from_user.id not in SUDOERS:
+                return await CallbackQuery.answer(
+                    "Bot is under maintenance. Please wait for some time...",
+                    show_alert=True,
+                )
         try:
             language = await get_lang(CallbackQuery.message.chat.id)
             _ = get_string(language)
