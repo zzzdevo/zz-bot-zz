0602e93eccb0cfb42a89521290958b563fe42b27
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-19 16:04:31 +0300

    Update resume.py

--- AnonX/plugins/resume.py
+++ AnonX/plugins/resume.py
@@ -1,21 +1,22 @@
-
 from pyrogram import filters
 from pyrogram.types import Message
-from strings.filters import command
+
 from config import BANNED_USERS
 from strings import get_command
 from AnonX import app
+from strings.filters import command
 from AnonX.core.call import Anon
 from AnonX.utils.database import is_music_playing, music_on
 from AnonX.utils.decorators import AdminRightsCheck
+from AnonX.utils.inline.play import close_keyboard
 
 # Commands
 RESUME_COMMAND = get_command("RESUME_COMMAND")
 
 
 @app.on_message(
-    command(RESUME_COMMAND)
-
+    filters.command(RESUME_COMMAND)
+    & ~filters.private
     & ~BANNED_USERS
 )
 @AdminRightsCheck
@@ -25,7 +26,8 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Anon.resume_stream(chat_id)
+    await Mukesh.resume_stream(chat_id)
     await message.reply_text(
-        _["admin_4"].format(message.from_user.mention)
+        _["admin_4"].format(message.from_user.first_name),
+        reply_markup=close_keyboard
     )
