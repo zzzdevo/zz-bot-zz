ee17a539f20001b80101ac92ef716834c5ee35d9
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-09-30 10:35:05 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -29,20 +29,20 @@ for filename in os.listdir(r"./strings"):
 
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+        languages["ckb"] = yaml.safe_load(
+            open(r"./strings/langs/ckb.yml", encoding="utf8")
         )
-        languages_present["en"] = languages["en"]["name"]
+        languages_present["ckb"] = languages["ckb"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
+        if language_name == "ckb":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["en"]:
+        for item in languages["ckb"]:
             if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
+                languages[language_name][item] = languages["ckb"][item]
     try:
         languages_present[language_name] = languages[language_name][
             "name"
