56504aa9d51cb0775ea6ab506cbfdd51a72ba51d
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-08 22:33:31 +0300

    Update ping.py

--- AnonX/plugins/ping.py
+++ AnonX/plugins/ping.py
@@ -1,22 +1,22 @@
 from datetime import datetime
-
+from strings.filters import command
 from pyrogram import filters
 from pyrogram.types import Message
-
+from strings.filters import command
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
 from AnonX import app
 from AnonX.core.call import Anon
 from AnonX.utils import bot_sys_stats
 from AnonX.utils.decorators.language import language
-from AnonX.utils.inline.play import close_keyboard
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
 
 
 @app.on_message(
-    filters.command(PING_COMMAND)
+    command(PING_COMMAND)
+    & ~BANNED_USERS
 )
 @language
 async def ping_com(client, message: Message, _):
@@ -25,12 +25,11 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"],
     )
     start = datetime.now()
-    pytgping = await Mukesh.ping()
+    pytgping = await Anon.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
         _["ping_2"].format(
-            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
-        ),
-        reply_markup=close_keyboard
+            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
+        )
     )
