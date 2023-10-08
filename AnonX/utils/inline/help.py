c54f6c6017335d7ce4e59839ca4f7b8fbad90f18
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-08 21:27:30 +0300

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from YukkiMusic import app
+from AnonX import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
