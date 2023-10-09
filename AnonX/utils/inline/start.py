421a3bc23db1962d6e6d0d76ea4cbba0bc123ee3
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-09 20:55:10 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config
 
 
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def start_pannel(_, BOT_USERNAME):
     buttons = [
         [
             InlineKeyboardButton(
@@ -26,31 +26,37 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     return buttons
 
 
+# noinspection PyTypeChecker
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_4"], url="https://t.me/MGIMT",
-            ),
-
-            InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            ),
-
-            InlineKeyboardButton(
-                text=_["S_B_7"], user_id=OWNER)
+            )
         ],
         [
-
             InlineKeyboardButton(
                 text=_["S_B_8"], callback_data="settings_back_helper"
             )
         ],
         [
+            InlineKeyboardButton(
+                text=_["S_B_4"], url=f"https://t.me/MGIMT"
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_3"], url=f"https://t.me/IQSUPP"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_7"], user_id=OWNER
+            )
 
+        ],
+        [
             InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG"
-                                 )
+            )
         ],
     ]
     return buttons
