7b9bf84451ad27e26746ec4e350de5065d954891
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-10 02:26:32 +0300

    Update Soundcloud.py

--- AnonX/platforms/Soundcloud.py
+++ AnonX/platforms/Soundcloud.py
@@ -1,19 +1,9 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import re
 from os import path
 
 from yt_dlp import YoutubeDL
 
-from YukkiMusic.utils.formatters import seconds_to_min
+from AnonX.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
